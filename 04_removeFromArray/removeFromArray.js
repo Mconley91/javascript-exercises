11f55dd459a98b0fbb5844596c598a69c1485c9d
mconley91 <matthew.e.conley@gmail.com>
2024-10-21 21:05:06 -0400

    complete 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...toBeRemoved) {
+    //return arr.filter((item)=> item != toBeRemoved); passes first test
+    let newarr = [];
+    outer: for (let i = 0; i < arr.length; i++){
+    for(let j = 0; j < toBeRemoved.length; j++){
+        if(arr[i] === toBeRemoved[j]){
+            continue outer;
+        };
+    };
+    newarr.push(arr[i]);
+   }
+   console.log(newarr);
+   return newarr;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;

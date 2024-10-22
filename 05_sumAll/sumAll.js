ba518110534e3915eeb4672ff5907b092f5c4f78
mconley91 <matthew.e.conley@gmail.com>
2024-10-22 16:12:48 -0400

    complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    let container = 0;
+    let startNum = firstNum < secondNum ? firstNum : secondNum;
+    let endNum = firstNum < secondNum ? secondNum : firstNum;
 
+    if(startNum < 0 || endNum < 0 || startNum != Math.round(startNum) || endNum != Math.round(endNum) || typeof startNum != 'number' || typeof endNum != 'number'){
+        return 'ERROR';
+    }
+    else{
+        for (let i = startNum; i <= endNum; i++){
+            container += i;
+        }
+        console.log(container);
+        return container;
+    }
 };
 
 // Do not edit below this line

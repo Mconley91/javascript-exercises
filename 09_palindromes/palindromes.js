15a4b93233306c01217cde8933f0e2019965dcac
mconley91 <matthew.e.conley@gmail.com>
2024-11-25 17:04:17 -0500

    completed 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const regex = /[A-Za-z0-9]/;
+    let splitString = string.toLowerCase()
+    .split('')
+    .filter((item)=> regex.test(item));
 
+    let forward = splitString.join('');
+    let backward = splitString.reverse().join('');
+    
+    return forward === backward;
 };
 
 // Do not edit below this line

80803c487d484ca77a2c24d4972fbd55ca15d075
mconley91 <matthew.e.conley@gmail.com>
2024-11-25 15:34:07 -0500

    complete calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total,item)=>total += item,0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	return array.reduce((total,item)=>total * item,1);
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+  let arr = [];
+  for(let i = 0; i < power; i++){
+    arr.push(number);
+  };
+  return arr.reduce((total,item)=>total * item,1);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let arr = [];
+  for(let i = 1; i < num+1; i++){
+    arr.push(i);
+  };
+  return arr.reduce((total,item)=>total * item,1);
 };
 
 // Do not edit below this line

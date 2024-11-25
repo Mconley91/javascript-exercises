f34458d316952ee1e9298e0abf68116eb9f57179
mconley91 <matthew.e.conley@gmail.com>
2024-11-25 17:44:19 -0500

    complete 10_fibonacci aha haaa

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    let fib = [0,1];
+    if(index < 0){
+        return "OOPS";
+    }
+    for(let i = 1; i < index; i++){
+        fib.push(fib[i-1] + fib[i]);
+    }
+return fib[index];
 };
 
 // Do not edit below this line

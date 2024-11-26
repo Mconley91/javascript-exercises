7858ba446bb2db10f848cf7436c403ca6f29d8e6
mconley91 <matthew.e.conley@gmail.com>
2024-11-25 19:42:35 -0500

    complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(arrayOfObjects) {
+const date = new Date();
 
+let sortedArray = arrayOfObjects.sort((obj1,obj2)=>{
+    let firstPerson = (obj1.yearOfDeath || date.getFullYear()) - obj1.yearOfBirth;
+    let secondPerson = (obj2.yearOfDeath || date.getFullYear()) - obj2.yearOfBirth;
+    return firstPerson > secondPerson ? -1 : 1;
+});
+
+return sortedArray[0];
 };
 
 // Do not edit below this line
